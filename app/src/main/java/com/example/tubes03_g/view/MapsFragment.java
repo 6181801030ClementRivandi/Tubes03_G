f257b17f06887eac45244da4f0a46a0c3a1f4443
Clement Rivandi <6181801030@student.unpar.ac.id>
2020-12-28 15:48:29 +0700

    Revert "Revert "Merge branch 'master' into orkz""
    This reverts commit a22e4c4644dd24acf24fc7ae39f233af4929b73b.

--- app/src/main/java/com/example/tubes03_g/view/MapsFragment.java
+++ app/src/main/java/com/example/tubes03_g/view/MapsFragment.java
@@ -1,4 +1,4 @@
-package com.example.tubes03_g;
+package com.example.tubes03_g.view;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,6 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.example.tubes03_g.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
